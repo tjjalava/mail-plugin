d0144da1da2951c6c7eaef06f2a2633018fa859f
Piotr Adamski <mcveat@gmail.com>
2012-11-21 23:32:34 +0100

    api published

---
+++ api/index.js
@@ -0,0 +1,1 @@
+Index.PACKAGES = {"mail" : [{"class" : "mail\/Mail.html", "object" : "mail\/Mail$.html", "name" : "mail.Mail"}, {"object" : "mail\/MailActor$.html", "name" : "mail.MailActor"}, {"class" : "mail\/MailPlugin.html", "name" : "mail.MailPlugin"}]};
